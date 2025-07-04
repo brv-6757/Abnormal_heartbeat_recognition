b72e0e2e744019ba7631a23a41b1abe99421998a
brv-6757 <154030862+brv-6757@users.noreply.github.com>
2025-07-04 07:28:50 +0530

    Delete Raspberry_pi/IOT_Project_Files directory

--- Raspberry_pi/IOT_Project_Files/testing.py
+++
@@ -1,9 +0,0 @@
-from modelcode import AutoEncoder,intrpl
-import numpy as np
-ae = AutoEncoder()
-ecg_data = np.load("test_data.npy")
-print(ecg_data.shape)
-
-for i in range(8):
-    result = ae.call(intrpl(ecg_data[i].reshape(1,100)))
-    print(result)
